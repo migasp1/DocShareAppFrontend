27eaa8218919d1b05c194dca1947b3ac9aa52cac
Miguel Ferreira <miguelpsferreira@hotmail.com>
2020-04-26 23:57:38 +0100

    Routing in account menu added

--- DocShareAppFrontend-app/src/app/app.module.ts
+++ DocShareAppFrontend-app/src/app/app.module.ts
@@ -9,11 +9,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
